2842ab680ce1c258d99946f9831d0aadba79f2d6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-09 00:57:20 +0530

    Updated backend: initialed id and be autogenerated

--- backend/src/main/java/lk/ijse/backend/entity/impl/Task.java
+++ backend/src/main/java/lk/ijse/backend/entity/impl/Task.java
@@ -1,6 +1,24 @@
 package lk.ijse.backend.entity.impl;
 
+import jakarta.persistence.*;
 import lk.ijse.backend.entity.SuperEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "Task")
 public class Task implements SuperEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
 }
