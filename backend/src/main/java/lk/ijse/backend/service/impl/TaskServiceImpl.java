6d2e480d724abb5ec7b5f0ad4c572c4571c43d85
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-09 01:19:22 +0530

    Updated backend: create TaskServiceIMPL class and implement the relevant interface

---
+++ backend/src/main/java/lk/ijse/backend/service/impl/TaskServiceImpl.java
@@ -0,0 +1,34 @@
+package lk.ijse.backend.service.impl;
+
+import lk.ijse.backend.dto.TaskStatus;
+import lk.ijse.backend.dto.impl.TaskDTO;
+import lk.ijse.backend.service.TaskService;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public void saveTask(TaskDTO noteDTO) {
+        
+    }
+
+    @Override
+    public List<TaskDTO> getAllTasks() {
+        return List.of();
+    }
+
+    @Override
+    public TaskStatus getTask(String noteId) {
+        return null;
+    }
+
+    @Override
+    public void deleteTask(String noteId) {
+
+    }
+
+    @Override
+    public void updateTask(String noteId, TaskDTO noteDTO) {
+
+    }
+}
