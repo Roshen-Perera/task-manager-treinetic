00c010a57485f365638f755467e59968de5b9ee0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-09 01:16:50 +0530

    Updated backend: created TaskServices interface

---
+++ backend/src/main/java/lk/ijse/backend/service/TaskService.java
@@ -0,0 +1,14 @@
+package lk.ijse.backend.service;
+
+import lk.ijse.backend.dto.TaskStatus;
+import lk.ijse.backend.dto.impl.TaskDTO;
+
+import java.util.List;
+
+public interface TaskService {
+    void saveTask(TaskDTO noteDTO);
+    List<TaskDTO> getAllTasks();
+    TaskStatus getTask(String noteId);
+    void deleteTask(String noteId);
+    void updateTask(String noteId, TaskDTO noteDTO);
+}
