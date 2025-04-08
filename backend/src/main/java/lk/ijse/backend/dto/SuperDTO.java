3e9d2895e78cb4d96e07b4f823f726e21ead51ff
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-09 01:03:01 +0530

    Updated backend: created taskDTO with fields

--- backend/src/main/java/lk/ijse/backend/dto/SuperDTO.java
+++ backend/src/main/java/lk/ijse/backend/dto/SuperDTO.java
@@ -1,4 +1,6 @@
 package lk.ijse.backend.dto;
 
-public class SuperDTO {
+import java.io.Serializable;
+
+public interface SuperDTO extends Serializable {
 }
---
+++ backend/src/main/java/lk/ijse/backend/dto/impl/TaskDTO.java
@@ -0,0 +1,11 @@
+package lk.ijse.backend.dto.impl;
+
+import java.time.LocalDateTime;
+
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+}
