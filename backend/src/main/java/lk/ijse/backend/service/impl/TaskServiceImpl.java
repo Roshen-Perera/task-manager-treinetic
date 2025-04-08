f212e827cff5023600ffcd712dbf3c501e5b59bc
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-09 01:24:20 +0530

    Updated backend: injected two beans mapping and taskDao

--- backend/src/main/java/lk/ijse/backend/service/impl/TaskServiceImpl.java
+++ backend/src/main/java/lk/ijse/backend/service/impl/TaskServiceImpl.java
@@ -1,12 +1,23 @@
 package lk.ijse.backend.service.impl;
 
+import jakarta.transaction.Transactional;
+import lk.ijse.backend.dao.TaskDAO;
 import lk.ijse.backend.dto.TaskStatus;
 import lk.ijse.backend.dto.impl.TaskDTO;
 import lk.ijse.backend.service.TaskService;
+import lk.ijse.backend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class TaskServiceImpl implements TaskService {
+    @Autowired
+    private TaskDAO taskDao;
+    @Autowired
+    private Mapping fieldMapping;
     @Override
     public void saveTask(TaskDTO noteDTO) {
 
