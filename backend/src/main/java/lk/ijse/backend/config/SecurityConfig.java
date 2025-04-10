d33e68c0cb4d5edbf379dba58bbea3351412e043
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-10 19:27:56 +0530

    Updated backend: Added a bean to decode passwords

--- backend/src/main/java/lk/ijse/backend/config/SecurityConfig.java
+++ backend/src/main/java/lk/ijse/backend/config/SecurityConfig.java
@@ -1,10 +1,29 @@
 package lk.ijse.backend.config;
 
+import lk.ijse.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserService userService;
+    private final JWTConfigFilter jwtConfigFilter;
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
