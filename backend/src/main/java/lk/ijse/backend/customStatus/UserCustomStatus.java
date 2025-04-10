66026056fd49b0d815ba1fb390440560e4e5d0bd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-10 17:46:04 +0530

    Updated backend: Created Custom status code for user

---
+++ backend/src/main/java/lk/ijse/backend/customStatus/UserCustomStatus.java
@@ -0,0 +1,8 @@
+package lk.ijse.backend.customStatus;
+
+import lk.ijse.backend.dto.UserStatus;
+
+public class UserCustomStatus implements UserStatus {
+    private int statusCode;
+    private String statusMessage;
+}
