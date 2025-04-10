33eb41029ead400f6e1623b2d6b41a8b6cec1cd5
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-10 23:41:59 +0530

    Updated frontend: Implemented crud logic in controller

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TaskService} from '../../service/task.service';
+import {Task} from '../../model/task';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,6 +8,56 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  taskObject : Task = new Task();
+  taskList : Task[] = [];
+  currentDate: string | null = '';
+  currentStatus: string  = 'PENDING';
+
+  title = '';
+  description = '';
+
+  ngOnInit(): void {
+    this.taskObject = new Task();
+    this.taskList = [];
+    this.loadAllTask();
+  }
+
+  loadAllTask() {
+    this.taskService.getTask().subscribe(res => {
+      this.taskList = res
+    }, err => {
+      alert("Unable to load task");
+    })
+  }
+
+  addTask(task : Task) {
+    this.taskService.addTask(task).subscribe(res => {
+      this.ngOnInit()
+    }, err => {
+      alert(err.error.message);
+    });
+  }
+
+  updateTask() {
+    this.taskService.updateTask(this.taskObject).subscribe(res => {
+      this.ngOnInit()
+    }, err => {
+      alert("Failed to update task");
+    });
+  }
+
+  deleteTask(task: Task) {
+    this.taskService.deleteTask(task).subscribe(res => {
+      this.ngOnInit()
+    }, err => {
+      alert("Failed to delete task");
+    });
+  }
+
+  constructor(private taskService: TaskService) {
+
+  }
 
 }
