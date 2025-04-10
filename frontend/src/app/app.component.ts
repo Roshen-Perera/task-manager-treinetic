533bb304e89d78a3e2b52de98021b36db3b19c76
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-11 00:00:23 +0530

    Updated frontend: removed app.config.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,8 +4,9 @@ import {DashboardComponent} from './components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent],
+  imports: [DashboardComponent, RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
