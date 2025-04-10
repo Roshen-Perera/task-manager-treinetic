72fa687c4944128379b01fb8cb13f1c8b4961108
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-10 23:07:42 +0530

    Updated frontend: Created addTask with hardcorded authentication

--- frontend/src/app/service/task.service.ts
+++ frontend/src/app/service/task.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Task} from '../model/task';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,13 @@ export class TaskService {
 
   constructor(private http: HttpClient) {
     this.serviceURL = "http://localhost:8080/trienetic/api/task"
+  }
+
+  addTask(task: Task): Observable<Task> {
+    const headers = new HttpHeaders({
+      'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyb3NoZW4iLCJpYXQiOjE3NDQyOTYzOTYsImV4cCI6MTc0NDM4Mjc5Nn0.00q3wmyZiORg1y_RPC-q2hJo1ODOKANJIHtMXWF7TQI',  // Example of adding an authorization token
+    });
+
+    return this.http.post<Task>(this.serviceURL, task, { headers });
   }
 }
