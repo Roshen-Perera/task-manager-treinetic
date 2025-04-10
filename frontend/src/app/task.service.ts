806d605b9aa1384b478d67d6be1328d1cced67ce
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-10 21:36:23 +0530

    Updated frontend: Initialized Angular Project

---
+++ frontend/src/app/task.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'https://your-api-url.com/tasks'; // Replace with your actual API URL
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  createTask(task: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, task);
+  }
+
+  getTaskById(id: string | null): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+}
