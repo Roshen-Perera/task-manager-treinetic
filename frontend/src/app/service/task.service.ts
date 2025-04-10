af0794a1a711ea950d7377effbbdc77870248bf2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-10 22:22:24 +0530

    Updated frontend: Created a container in dashboard.component.html

--- frontend/src/app/service/task.service.ts
+++ frontend/src/app/service/task.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  serviceURL : string ;
+
+  constructor(private http: HttpClient) {
+    this.serviceURL = "http://localhost:8080/trienetic/api/task"
+  }
 }
