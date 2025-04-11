d9730d2fcd8c3b8fec9bd592691fb7e93fc3fe25
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-04-11 16:23:44 +0530

    Updated frontend: Resolved ui problems

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
 export const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'dashboard', component: DashboardComponent},
 ];
